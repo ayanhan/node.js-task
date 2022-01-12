8cef51e62cd407356afecbea660ea5fe9008eabf
ayanjk <kalkhozhaev.j@gmail.com>
2022-01-12 09:31:56 +0900

    server

---
+++ controllers/tasks.js
@@ -0,0 +1,24 @@
+const getAllTasks = (req, res) => {
+    res.send("get all tasks");
+}
+
+const getTask = (req, res) => {
+    res.send("get single task");
+}
+
+const createTask = (req, res) => {
+    res.json(req.body);
+}
+
+const updateTask = (req, res) => {
+    res.send("update task");
+}
+ 
+const deleteTask = (req, res) => {
+    res.send("delete task");
+}
+
+
+module.exports = {
+    getAllTasks,getTask,createTask,updateTask,deleteTask 
+} 
